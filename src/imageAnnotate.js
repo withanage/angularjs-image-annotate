bb870353e9d76e74cddc626ea1058504c0aee3eb
Jack Way <jack@nimbleworks.co.uk>
2013-12-20 13:28:55 +0000

    Line opacity can now be set via directive attribute

--- src/imageAnnotate.js
+++ src/imageAnnotate.js
@@ -42,29 +42,32 @@ angular.module('nimbleworks.imageAnnotate', []).directive('nimImageAnnotate', fu
             src: '@',
             width: '@',
             height: '@',
-            lineCol: '@',
-            lineWidth: '@'
+            lineOpacity: '@'
         },
-        template: '<div><div style="background: url({{ src }}); width: {{ width }}px; height: {{ height }}px; cursor: crosshair;"><canvas width="{{ width }}" height="{{ height }}" style="opacity: .33"></canvas></div><br /><div><button ng-click="clear()">Clear</button></div></div>',
-        link: function (scope, element) {
-            var $canvas;
-            lineCol = scope.lineCol || 'rgba(255, 255, 0, 1)';
-            lineWidth = scope.lineWidth || 20;
-            canvas = element.find('canvas')[0];
-            context = canvas.getContext('2d');
-            $canvas = angular.element(canvas);
-            clear();
-            $canvas.on('mousedown', function () {
-                mouseIsDown = true;
-            });
-            $canvas.on('mouseup', function () {
-                mouseIsDown = false;
-                scope.ngModel = canvas.toDataURL();
-            });
-            $canvas.on('mousemove', function (evt) {
-                draw(evt);
-            });
-            scope.clear = clear;
+        template: '<div><div style="background: url({{ src }}); width: {{ width }}px; height: {{ height }}px; cursor: crosshair;"><canvas width="{{ width }}" height="{{ height }}" style="opacity: {{ lineOpacity }}"></canvas></div><br /><div><button ng-click="clear()">Clear</button></div></div>',
+        compile: function (element, attributes) {
+            attributes.lineOpacity = attributes.lineOpacity || '.33';
+            attributes.lineCol = attributes.lineCol || 'rgba(255, 255, 0, 1)';
+            attributes.lineWidth = attributes.lineWidth || 20;
+            return function (scope, element) {
+                var $canvas;
+                lineCol = attributes.lineCol;
+                lineWidth = attributes.lineWidth;
+                canvas = element.find('canvas')[0];
+                context = canvas.getContext('2d');
+                $canvas = angular.element(canvas);
+                $canvas.on('mousedown', function () {
+                    mouseIsDown = true;
+                });
+                $canvas.on('mouseup', function () {
+                    mouseIsDown = false;
+                    scope.ngModel = canvas.toDataURL();
+                });
+                $canvas.on('mousemove', function (evt) {
+                    draw(evt);
+                });
+                scope.clear = clear;
+            }
         }
     };
     return directive;
